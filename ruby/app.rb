2c09a4885960d592e20f2d637494589299e9edb1
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-27 12:34:28 +0900

    alter

--- ruby/app.rb
+++ ruby/app.rb
@@ -372,5 +372,7 @@ SQL
     db.query("DELETE FROM entries WHERE id > 500000")
     db.query("DELETE FROM comments WHERE id > 1500000")
     #db.query("alter table footprints add index idx_footprints(`user_id`, `owner_id`, `created_at`)")
+    #alter table relations add index idx_one(one);
+    #alter table relations add index idx_another(another);
   end
 end
