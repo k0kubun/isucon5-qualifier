77253512287090a02281f4c854f3d9e4f8ed2708
isucon <isucon@isucon.net>
2015-09-27 11:03:18 +0900

    logging

--- ruby/unicorn_config.rb
+++ ruby/unicorn_config.rb
@@ -2,3 +2,5 @@ worker_processes 1
 preload_app true
 listen 8080
 pid "/home/isucon/webapp/ruby/unicorn.pid"
+stderr_path '/home/isucon/logs/unicorn_stderr.log'
+stdout_path '/home/isucon/logs/unicorn_stdout.log'
