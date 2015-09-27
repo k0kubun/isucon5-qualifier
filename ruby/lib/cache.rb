e418afdf681bbce47238d81edb8e680c11729a9d
isucon <isucon@isucon.net>
2015-09-27 16:40:39 +0900

    use unixsocket as redis

--- ruby/lib/cache.rb
+++ ruby/lib/cache.rb
@@ -7,7 +7,7 @@ class Cache
     end
 
     def make_client
-      if false #File.exist?("/tmp/redis.sock")
+      if File.exist?("/tmp/redis.sock")
         Redis.new(
           path: "/tmp/redis.sock",
           db: 'isucon5q'
