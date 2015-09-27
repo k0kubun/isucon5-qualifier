1dd85fdf37ad768ac0ce46c25db80d0744c6499c
Takashi Kokubun <takashikkbn@gmail.com>
2015-09-27 17:42:08 +0900

    eager load entry

--- ruby/app.rb
+++ ruby/app.rb
@@ -125,6 +125,15 @@ SQL
       records
     end
 
+    def eager_load_entry(comments)
+      entry_ids = comments.map { |c| c[:entry_id] }
+      db.xquery("SELECT * FROM entries WHERE id IN (#{entry_ids.join(',')})").each do |entry|
+        comment = comments.find { |c| c[:entry_id] == entry[:id] }
+        comment[:entry] = entry if comment
+      end
+      comments
+    end
+
     def user_from_account(account_name)
       user = db.xquery('SELECT * FROM users WHERE account_name = ? LIMIT 1', account_name).first
       raise Isucon5::ContentNotFound unless user
@@ -276,11 +285,10 @@ SQL
     end
 
     comments_of_friends = []
-    db.query("SELECT * FROM comments WHERE user_id IN (#{friend_ids.join(',')}) ORDER BY id DESC LIMIT 10").each do |comment|
-      entry = db.xquery('SELECT * FROM entries WHERE id = ? LIMIT 1', comment[:entry_id]).first
+    eager_load_entry(db.query("SELECT * FROM comments WHERE user_id IN (#{friend_ids.join(',')}) ORDER BY id DESC LIMIT 10")).each do |comment|
+      entry = comment[:entry]
       entry[:is_private] = (entry[:private] == 1)
       next if entry[:is_private] && !permitted?(entry[:user_id])
-      comment[:entry] = entry
       comments_of_friends << comment
     end
 
